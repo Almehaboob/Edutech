24c4f3d2539b9e400535ca8230e38c906cabe0ac
Almehaboob <allualmehboob10@gmail.com>
2024-12-16 15:32:13 +0530

    added more fileds into course and validates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_16_074031) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_16_084544) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -58,6 +58,10 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_16_074031) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.text "short_description"
+    t.string "languaes", default: "English", null: false
+    t.string "level", default: "Beginner", null: false
+    t.integer "price", default: 0, null: false
     t.index ["user_id"], name: "index_courses_on_user_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,15 @@
-# User.create!(email: 'admin@example.com', password: 'admin@example.com', password_confirmation: 'admin@example.com')
+# Create an admin user
+User.create!(email: 'admin@example.com', password: 'admin@example.com', password_confirmation: 'admin@example.com')
 
-
+# Seed Courses
 30.times do
-    Course.create!([{
-      title: Faker::Educator.course_name,
-      description: Faker::JapaneseMedia::OnePiece.quote,
-      user_id: User.first.id
-      }])
-  end
+  Course.create!({
+    title: Faker::Educator.course_name,
+    description: Faker::JapaneseMedia::OnePiece.quote,
+    short_description: Faker::Lorem.sentence(word_count: 10), # A short description with 10 words
+    languaes: Faker::Lorem.words(number: 2).join(", "),  # Random languages from a list
+    level: Faker::Educator.degree,  # Random level (e.g., 'Associate', 'Bachelor', etc.)
+    price: Faker::Number.decimal(l_digits: 2, r_digits: 2), # Random price with 2 decimal places
+    user_id: User.first.id
+  })
+end
