10db55b37b4abd04bbbe6f4d5331bd66d331f0f6
Almehaboob <allualmehboob10@gmail.com>
2024-12-18 11:33:37 +0530

    autherizes courses and lesson using pundit gem

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,6 +1,16 @@
 class Lesson < ApplicationRecord
+
   belongs_to :course
   validates :title, :content, :course, presence: true
-  # extend FriendlyId
-  # friendly_id :title, use: :slugged
+  
+  include PublicActivity::Model
+  tracked owner: Proc.new { |_controller, model| model.owner }
+  attr_accessor :owner # Allows explicit assignment of the owner
+
+  has_rich_text :content
+
+
+  def to_s
+    title
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   resources :lessons
   resources :titles
   devise_for :users
-  resources :courses
-  resources :lessons
   resources :users, only: [:index,  :edit, :show, :update ]
-
+  
+  resources :courses do
+    resources :lessons
+  end
+  
   root "home#index"
   get 'home/home'
   get 'home/activity'
